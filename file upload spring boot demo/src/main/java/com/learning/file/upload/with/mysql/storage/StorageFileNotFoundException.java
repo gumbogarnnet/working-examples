07418cac8efeaf9b8d33a992fd01e463244d3cd3
Garnnet Gumbo <ggumbo@psmi.co.zw>
2020-01-14 16:18:28 +0200

    initial

---
+++ file upload spring boot demo/src/main/java/com/learning/file/upload/with/mysql/storage/StorageFileNotFoundException.java
@@ -0,0 +1,12 @@
+package com.learning.file.upload.with.mysql.storage;
+
+public class StorageFileNotFoundException extends StorageException {
+
+	public StorageFileNotFoundException(String message) {
+		super(message);
+	}
+
+	public StorageFileNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
---
+++ file upload spring boot demo/src/test/java/com/learning/file/upload/with/mysql/FileUploadWithMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.file.upload.with.mysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileUploadWithMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
