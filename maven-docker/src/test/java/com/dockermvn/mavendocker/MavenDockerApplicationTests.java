641fe3d90e1cca8fba318e4f027e21697ff6cb99
Your Name <you@example.com>
2023-09-20 09:30:47 +0200

    maven docker full working

---
+++ maven-docker/src/test/java/com/dockermvn/mavendocker/MavenDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dockermvn.mavendocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
