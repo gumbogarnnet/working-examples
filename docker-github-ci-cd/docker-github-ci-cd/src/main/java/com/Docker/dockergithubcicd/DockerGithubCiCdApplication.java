f5764318d1426d32e2d1a8fb557eb98d1f504a82
Your Name <you@example.com>
2023-09-28 15:33:59 +0200

    ci-cd

---
+++ docker-github-ci-cd/docker-github-ci-cd/src/main/java/com/Docker/dockergithubcicd/DockerGithubCiCdApplication.java
@@ -0,0 +1,13 @@
+package com.Docker.dockergithubcicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerGithubCiCdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerGithubCiCdApplication.class, args);
+	}
+
+}
---
+++ docker-github-ci-cd/docker-github-ci-cd/src/test/java/com/Docker/dockergithubcicd/DockerGithubCiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Docker.dockergithubcicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerGithubCiCdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
